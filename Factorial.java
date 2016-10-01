c2d47c53a1237fe066a0966714d1ef1269c2649b
Hanif Mirza <hanif0088@gmail.com>
2016-10-01 15:21:47 -0400

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -27,4 +27,13 @@ public class Factorial
     
          return fact;
      }
+     
+     public static int factorial(int number)
+     {
+         if(number == 1 || number == 0)
+         {
+             return 1;
+         }
+         return number*factorial(number - 1);
+     }
 }
