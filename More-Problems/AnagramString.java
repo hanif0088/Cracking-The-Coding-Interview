001ab3c52fe11077bd7d95ed8c8cf4203d4f10f8
Hanif Mirza <hanif0088@gmail.com>
2016-10-06 01:16:55 -0400

    Create AnagramString.java

---
+++ More-Problems/AnagramString.java
@@ -0,0 +1,22 @@
+/*two String are called anagram, if they contains same characters but on different order 
+e.g. army and mary, stop and pots etc. */
+
+import java.util.Arrays;
+
+public class AnagramString
+{
+     public static void main(String []args)
+     {
+        System.out.println( isAnagram("hanif", "finha") );
+     }
+     
+     public static boolean isAnagram(String word, String anagram)
+     {
+         char[] wordArray = word.toCharArray();
+         char[] anagramArray = anagram.toCharArray();
+         Arrays.sort(wordArray);
+         Arrays.sort(anagramArray);
+         
+         return Arrays.equals(wordArray, anagramArray);
+     }
+}
