8c12221c611686c4450f41bf35f360b6c5a2cb6c
Hanif Mirza <hanif0088@gmail.com>
2016-11-19 18:21:18 -0500

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,19 +1,36 @@
-public class PrimeNumber
+public class PrimeNumber 
 {
-     public static void main(String []args)
-     {
-        System.out.println("Hello World");
-        System.out.println( isPrimeNumber(4)) ;
-     }
-     static boolean isPrimeNumber(int number)
-     {
-         for(int x=2; x < number; x++)
-         {
-             if((number % x) == 0)
-             {
-                 return false;
-             }
-         }
-         return true;
-     }
+	public static void main(String[] args) 
+	{
+		System.out.println( sumOfPrimeNumbers(100) );
+	}
+     
+	static boolean isPrimeNumber(int number)
+	{
+		for(int x=2; x < number/2; x++)
+		{
+			if( number%x == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	//Sum of first 1000 prime numbers, where size = 1000
+	static int sumOfPrimeNumbers(int size)
+	{
+		int sum = 0;
+		int number = 2;
+		int count = 1;
+		while(count <= size)
+		{
+			if( isPrimeNumber(number) )
+			{
+				sum = sum + number;
+				count++;
+			}
+			number++;
+		}
+		return sum;
+	}
 }
